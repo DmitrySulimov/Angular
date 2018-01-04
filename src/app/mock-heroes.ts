733ca8b01ff993b3d367596c2edb10ed2d2cd5ce
Дмитрий Сулимов <sulimovdmitryvnua@gmail.com>
2018-01-04 17:58:44 +0200

    git init and added module heroes, mock repository, class heroes; fix styles

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Dmitry' },
+  { id: 2, name: 'Narcoman Yurii' },
+  { id: 3, name: 'Processor Intel' },
+  { id: 4, name: 'Professor X' },
+  { id: 5, name: 'Magneto' },
+  { id: 6, name: 'Rubben' },
+  { id: 7, name: 'Arnold' },
+  { id: 8, name: 'Dr ZLO' },
+  { id: 9, name: 'Helga Pataki' },
+  { id: 10, name: 'Tornado' }
+];
