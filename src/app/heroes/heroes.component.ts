346745436316e616b2b002929b4fa12e4f1450a7
Дмитрий Сулимов <sulimovdmitryvnua@gmail.com>
2018-01-09 18:55:44 +0200

    add dashboard, massages.service and message component, realize routing, add some styles

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -11,21 +12,16 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  selectedHero: Hero
-
- getHeroes(): void { 
-        this.heroes = this.heroService.getHeroes();
-        }
-
 
    constructor(private heroService: HeroService) { }
+
+ getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
 
   ngOnInit() {
   this.getHeroes();
   }
-
- onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-        }
 
 }
